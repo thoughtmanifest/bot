ed248b91951d9b5b29f2fac95fc935e1f4221a2b
David Sison <daviddsison@gmail.com>
2016-07-24 10:34:06 -0400

    oconn bot

--- bot.clj
+++ bot.clj
@@ -1,6 +1,11 @@
+(ns bot)
+
 (defn run
-  [{:keys [arena state bot_id energy spawn-bot?] :as step-details}]
-  {:commands [{:cmd "MOVE"
-               :metadata {:direction (rand-nth [0 1 2 3 4])}}
-              {:cmd "SET_STATE"
-               :metadata {:foo "bar"}}]})
+  [{:keys  [arena state bot_id energy spawn-bot?] :as step-details}]
+  (let [command-options [[{:cmd  "SHOOT"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7]) :energy 20}}
+                          {:cmd  "SET_STATE"
+                           :metadata  {:foo  "bar"}}]
+                         [{:cmd  "MOVE"
+                           :metadata  {:direction (rand-nth  [0 1 2 3 4 5 6 7])}}]]]
+    {:commands (rand-nth command-options)}))
